ef3c1d8376c3b111bb845d3d6e2965e3625e7ced
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-07-09 21:52:46 +0700

    Buổi 1

---
+++ roadmap.js
@@ -0,0 +1,90 @@
+const roadmap = {
+   MySQL: {
+      "Buổi 1": "Khải giảng",
+      "Buôi 2": "Basic SQL",
+      "Buổi 3": "Query SQL, Relationshiop"
+   },
+   ExpressJS: {
+      "Buổi 4": [
+         "- Làm quen với ExpressJS",
+         "- MySQL2, Kết nối BE với Database (DB)",
+         "- Kết nối 3 hệ thống lại với nhau FE, BE, DB",
+         "- CORS"
+      ],
+      "Buổi 5": [
+         "- ORM Sequelize",
+         "- Code First, Database First",
+         "- ORM Prisma"
+      ],
+      "Buổi 6": [
+         "- Áp dụng mô hình MVC"
+      ],
+      "Buổi 7": [
+         "- Xử lý lỗi"
+      ]
+   },
+   Auth: {
+      "Buổi 8": [
+         "- Xây dưng api đăng nhập, đăng ký",
+         "- Mã hoá password",
+         "- Login google, demo hack facebook"
+      ],
+      "Buổi 9": [
+         "- Access Token (jsonwebtoken)",
+         "- Cấu truc của Acces Token",
+         "- Gửi email",
+      ],
+      "Buổi 10": [
+         "- Refresh Token (jsonwebtoken)",
+         "- Middleware bảo vệ API"
+      ],
+      "Buổi 11": [
+         "- Phần quyền",
+         "- Role, Permission"
+      ],
+      "Buổi 12": [
+         "- Viết unit test với jest",
+         "- Debug trên vscode",
+         "- Swagger"
+      ]
+   },
+   Prisma: {
+      "Buổi 13": [
+         "- Xây dựng api get-info",
+         "- Chức năng đăng nhập với Google Authenticator"
+      ],
+      "Buổi 14": [
+         "- GraphQL",
+         "- Upload hình local/cloud",
+         "- Ráp Api và FE"
+      ],
+      "Buổi 15": [
+         "- Socket IO",
+         "- Xây dựng chức năng chat"
+      ]
+   },
+   NestJS: {
+      "Buổi 16": [
+         "- Cấu trúc module",
+         "- Upload hình",
+         "- Swagger"
+      ],
+      "Buổi 17": [
+         "- Xử lý Token với passport, strategy"
+      ]
+   },
+   Docker: {
+      "Buổi 18": [
+         "- Tìm hiểu docker",
+         "- docker compose"
+      ],
+      "Buổi 19": [
+         "- Deploy hoàn chỉnh lên server",
+         "- CI/CD"
+      ]
+   }
+}
+
+// - callback function
+// - Promise và then().catch(), async await
+// - try..catch
